6bb30d37c75f7711b3743325d61452be0af4299c
Ciro Dias <ciro.d.dias@gmail.com>
2021-11-14 00:53:59 -0300

    Content and SideBar componentized

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react';
 
-import { Button } from './components/Button';
-import { MovieCard } from './components/MovieCard';
+import { IGenreResponseProps } from './components/interfaces/IGenreResponseProps';
+import { IMovieProps } from './components/interfaces/IMovieProps';
 
-// import { SideBar } from './components/SideBar';
-// import { Content } from './components/Content';
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
 
 import { api } from './services/api';
 
@@ -13,43 +13,26 @@ import './styles/global.scss';
 import './styles/sidebar.scss';
 import './styles/content.scss';
 
-interface GenreResponseProps {
-  id: number;
-  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
-  title: string;
-}
-
-interface MovieProps {
-  imdbID: string;
-  Title: string;
-  Poster: string;
-  Ratings: Array<{
-    Source: string;
-    Value: string;
-  }>;
-  Runtime: string;
-}
-
 export function App() {
   const [selectedGenreId, setSelectedGenreId] = useState(1);
 
-  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+  const [genres, setGenres] = useState<IGenreResponseProps[]>([]);
 
-  const [movies, setMovies] = useState<MovieProps[]>([]);
-  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+  const [movies, setMovies] = useState<IMovieProps[]>([]);
+  const [selectedGenre, setSelectedGenre] = useState<IGenreResponseProps>({} as IGenreResponseProps);
 
   useEffect(() => {
-    api.get<GenreResponseProps[]>('genres').then(response => {
+    api.get<IGenreResponseProps[]>('genres').then(response => {
       setGenres(response.data);
     });
   }, []);
 
   useEffect(() => {
-    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
+    api.get<IMovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
       setMovies(response.data);
     });
 
-    api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
+    api.get<IGenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
       setSelectedGenre(response.data);
     })
   }, [selectedGenreId]);
@@ -60,36 +43,10 @@ export function App() {
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <nav className="sidebar">
-        <span>Watch<p>Me</p></span>
+      
+      <SideBar genres = {genres} selectedGenreId = {selectedGenreId} handleClickButton = {handleClickButton}/>
+      <Content selectedGenre = {selectedGenre} movies = {movies}/>
 
-        <div className="buttons-container">
-          {genres.map(genre => (
-            <Button
-              key={String(genre.id)}
-              title={genre.title}
-              iconName={genre.name}
-              onClick={() => handleClickButton(genre.id)}
-              selected={selectedGenreId === genre.id}
-            />
-          ))}
-        </div>
-
-      </nav>
-
-      <div className="container">
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
-      </div>
     </div>
   )
 }
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,26 @@
-export function Content() {
-  // Complete aqui
+import { IGenreResponseProps } from './interfaces/IGenreResponseProps'
+import { IMovieProps } from './interfaces/IMovieProps'
+import { MovieCard } from './MovieCard'
+
+interface IContentProps {
+  selectedGenre: IGenreResponseProps,
+  movies: IMovieProps[]
+}
+
+export function Content(props: IContentProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {props.selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {props.movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
